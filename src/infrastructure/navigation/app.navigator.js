4f2555b89baaeb02aa986e64b463287bf29ed7ec
Hanna Lam <hannalam.momo@gmail.com>
2023-03-06 00:58:32 +0100

    setting page update

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -2,9 +2,11 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { MatchesScreen } from "../../features/clients/screens/matches.screen";
 import { SafeArea } from "../../components/utility/safe-area.component";
+import styled from "styled-components/native";
+import { List, Avatar } from "react-native-paper";
 
 const Tab = createBottomTabNavigator();
 
@@ -16,9 +18,49 @@ const TAB_ICON = {
   Settings: "md-settings-outline",
 };
 
+const AvatarContainer = styled.View`
+  align-items: center;
+`;
+
 const Profile = () => (
   <SafeArea>
-    <Text>Profile</Text>
+    <AvatarContainer>
+      <Avatar.Icon size={100} icon="human" backgroundColor="#2182BD" />
+      <View style={{ paddingTop: 5 }} />
+      <Text variant="label" style={{ padding: 5 }}>
+        John Jason
+      </Text>
+      <Text variant="label">Data Analyst</Text>
+    </AvatarContainer>
+
+    <List.Section>
+      <List.Item
+        style={{ padding: 5 }}
+        title="Identity"
+        description="I graduated from University of London"
+      />
+      <List.Item
+        style={{ padding: 5 }}
+        title="Work Experience"
+        description="0-1 year (fresh grad program"
+      />
+      <List.Item style={{ padding: 5 }} title="Programming Languages" />
+      <List.Item style={{ padding: 5 }} title="Skills" />
+      <List.Item style={{ padding: 5 }} title="Education" />
+      <List.Item
+        style={{ padding: 5 }}
+        title="Favorite"
+        description="View your saved job"
+        left={(props) => <List.Icon {...props} color="black" icon="book" />}
+        //onPress={() => navigation.navigate("Favourites")}
+      />
+      <List.Item
+        style={{ padding: 5 }}
+        title="Logout"
+        left={(props) => <List.Icon {...props} color="black" icon="door" />}
+        //onPress={onLogout}
+      />
+    </List.Section>
   </SafeArea>
 );
 const Chat = () => (
@@ -26,16 +68,19 @@ const Chat = () => (
     <Text>Chat</Text>
   </SafeArea>
 );
+
+/*
 const Bookmark = () => (
   <SafeArea>
     <Text>Saved Job</Text>
   </SafeArea>
 );
+
 const Settings = () => (
   <SafeArea>
     <Text>Settings</Text>
   </SafeArea>
-);
+); */
 
 const createScreenOptions = ({ route }) => {
   const iconName = TAB_ICON[route.name];
@@ -62,10 +107,8 @@ export const AppNavigator = () => (
       }}
     >
       <Tab.Screen name="Matches" component={MatchesScreen} />
-      <Tab.Screen name="Profile" component={Profile} />
       <Tab.Screen name="Chat" component={Chat} />
-      <Tab.Screen name="Bookmark" component={Bookmark} />
-      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   </NavigationContainer>
 );
